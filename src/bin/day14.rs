45f0cebe1b8819cd521de95118c3a23850148e25
David Stangl <david@david-stangl.com>
2023-12-15 17:24:24 +0100

    Optimize day 14 a bit

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -9,89 +9,72 @@ use itertools::Itertools;
 fn tilt(
     len_tilt: usize,
     len_non_tilt: usize,
-    mut set_or_get: impl FnMut(usize, usize, Option<u8>) -> u8,
+    grid: &mut [u8],
+    mut index: impl FnMut(usize, usize) -> usize,
 ) {
     for i in 0..len_non_tilt {
         let mut next = 0;
         for j in 0..len_tilt {
-            let c = set_or_get(j, i, None);
-            if c == b'#' {
+            let idx = index(j, i);
+            if grid[idx] == b'#' {
                 next = j + 1;
-            } else if c == b'O' {
-                set_or_get(j, i, Some(b'.'));
-                set_or_get(next, i, Some(b'O'));
+            } else if grid[idx] == b'O' {
+                grid[idx] = b'.';
+                grid[index(next, i)] = b'O';
                 next += 1;
             }
         }
     }
 }
 
-fn tilt_cycle(grid: &mut [Vec<u8>]) {
-    let height = grid.len();
-    let width = grid[0].len();
-    tilt(height, width, |y, x, c| {
-        grid[y][x] = c.unwrap_or(grid[y][x]);
-        grid[y][x]
-    });
-    tilt(width, height, |x, y, c| {
-        grid[y][x] = c.unwrap_or(grid[y][x]);
-        grid[y][x]
-    });
-    tilt(height, width, |y, x, c| {
-        let y = height - y - 1;
-        grid[y][x] = c.unwrap_or(grid[y][x]);
-        grid[y][x]
-    });
-    tilt(width, height, |x, y, c| {
-        let x = width - x - 1;
-        grid[y][x] = c.unwrap_or(grid[y][x]);
-        grid[y][x]
-    });
+fn tilt_cycle(grid: &mut [u8], height: usize, width: usize) {
+    tilt(height, width, grid, |y, x| y * width + x);
+    tilt(width, height, grid, |x, y| y * width + x);
+    tilt(height, width, grid, |y, x| (height - 1 - y) * width + x);
+    tilt(width, height, grid, |x, y| y * width + (width - 1 - x));
 }
 
-fn eval_grid(grid: &[Vec<u8>]) -> usize {
+fn eval_grid(grid: &[u8], height: usize, width: usize) -> usize {
     grid.iter()
         .enumerate()
-        .map(|(y, row)| row.iter().filter(|&&c| c == b'O').count() * (grid.len() - y))
+        .filter(|&(_, &c)| c == b'O')
+        .map(|(i, _)| (height - i / width))
         .sum()
 }
 
-fn part1(mut grid: Vec<Vec<u8>>) -> usize {
-    let height = grid.len();
-    let width = grid[0].len();
-    tilt(height, width, |y, x, c| {
-        if let Some(c) = c {
-            grid[y][x] = c;
-        }
-        grid[y][x]
-    });
-    eval_grid(&grid)
+fn part1(mut grid: Vec<u8>, height: usize, width: usize) -> usize {
+    tilt(height, width, &mut grid, |y, x| y * width + x);
+    eval_grid(&grid, height, width)
 }
 
-fn part2(mut grid: Vec<Vec<u8>>) -> usize {
-    let mut path = vec![grid.clone()];
+fn part2(mut grid: Vec<u8>, height: usize, width: usize) -> usize {
     let mut seen = HashMap::new();
     seen.insert(grid.clone(), 0);
     loop {
-        tilt_cycle(&mut grid);
-        if let Some(cycle_start) = seen.insert(grid.clone(), path.len()) {
-            let cycle_len = path.len() - cycle_start;
+        tilt_cycle(&mut grid, height, width);
+        if let Some(cycle_start) = seen.insert(grid.clone(), seen.len()) {
+            let cycle_len = seen.len() - cycle_start;
             let idx_in_cycle = (1_000_000_000 - cycle_start) % cycle_len;
-            break eval_grid(&path[cycle_start + idx_in_cycle]);
+            let idx_in_path = idx_in_cycle + cycle_start;
+            let (final_grid, _) = seen.iter().find(|&(_, &idx)| idx == idx_in_path).unwrap();
+            break eval_grid(final_grid, height, width);
         }
-        path.push(grid.clone());
     }
 }
 
 fn main() -> Result<()> {
+    let mut height = 0;
     let grid: Vec<_> = io::stdin()
         .lock()
         .lines()
+        .inspect(|_| height += 1)
         .map_ok(|line| line.into_bytes())
+        .flatten_ok()
         .collect::<Result<_, _>>()?;
+    let width = grid.len() / height;
 
-    let part1 = part1(grid.clone());
-    let part2 = part2(grid.clone());
+    let part1 = part1(grid.clone(), height, width);
+    let part2 = part2(grid.clone(), height, width);
 
     println!("Part 1: {part1}");
     println!("Part 2: {part2}");
